77eb2b6f9e5c6b333b67a9e8cc07ab234e94b785
OliPerraul <olivier.perrault@usherbrooke.ca>
2024-07-27 10:38:43 -0400

    add window

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,40 @@
 #include <iostream>
-
 #include <GLFW/glfw3.h>
 
 int main()
 {
-    int x = 0;
-    std::cout << "Hello world" << std::endl;
-    std::cin >> x;
+    // Initialize the library
+    if (!glfwInit())
+    {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
 
+    // Create a windowed mode window and its OpenGL context
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    // Make the window's context current
+    glfwMakeContextCurrent(window);
+
+    // Loop until the user closes the window
+    while (!glfwWindowShouldClose(window))
+    {
+        // Render here
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Swap front and back buffers
+        glfwSwapBuffers(window);
+
+        // Poll for and process events
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
